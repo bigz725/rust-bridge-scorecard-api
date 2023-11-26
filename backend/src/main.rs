c2e879d0ff717a7d44e1f527a6c8a68e9ddd5fac
Zach Kissane <zkissane@gmail.com>
2023-11-26 12:04:03 -0600

    add mongodb connection to state

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 
 use axum::{response::Json, routing::get, Router,};
+use mongodb::{Client, options::ServerAddress};
 use serde_json::{json, Value};
 
 async fn hello_world_handler() -> Json<Value> {
@@ -9,10 +10,35 @@ async fn hello_world_handler() -> Json<Value> {
 
 #[tokio::main]
 async fn main () {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    let app: Router<> = 
-        Router::new().route("/", get(hello_world_handler));
+    dotenv::dotenv().ok();
+
+    let db_conn = db_conn().await;
+    let port = dotenv::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let port_int = port.parse::<u16>().unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], port_int));
+    let app = 
+        Router::new()
+          .route("/", get(hello_world_handler))
+          .with_state(db_conn);
 
         axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+}
+
+async fn db_conn() -> Client {
+    let uri = dotenv::var("MONGODB_URL").unwrap_or_else(|_| "mongodb://localhost/bridge_scorecard_api".to_string());
+    let server_addresses = vec![ServerAddress::Tcp { host: uri, port: None}];
+    let min_pool_size = dotenv::var("MONGODB_MIN_POOL_SIZE").unwrap_or_else(|_| "1".to_string());
+    let max_pool_size = dotenv::var("MONGODB_MAX_POOL_SIZE").unwrap_or_else(|_| "10".to_string());
+    let (min_pool_size_int, max_pool_size_int) = (
+        min_pool_size.parse::<u32>().unwrap(),
+        max_pool_size.parse::<u32>().unwrap()
+    );
+    let options = mongodb::options::ClientOptions::builder()
+        .min_pool_size(min_pool_size_int)
+        .max_pool_size(max_pool_size_int)
+        .hosts(server_addresses)
+        .build();
+
+    Client::with_options(options).unwrap()
 }
 
