34a5f7e5cf5d4fdfb57d48b17317fa8df013c6e5
Zach Kissane <zkissane@gmail.com>
2023-12-17 12:17:56 -0600

    login works, roles appear as they do on node server, token now properly has timestamp

--- backend/src/web/routes_login.rs
+++ backend/src/web/routes_login.rs
@@ -1,11 +1,17 @@
-use crate::{Error, Result, AppState, auth::jwt::{Claims, create_token}};
-use axum::{Json, Router, routing::post, extract::State};
+use crate::{
+    auth::jwt::{create_token, Claims},
+    models::user::Role,
+    AppState, Error, Result,
+};
+use axum::{extract::State, routing::post, Json, Router};
 //use extract::State;
-use serde::Deserialize;
-use serde_json::{Value, json};
-use mongodb::bson::doc;
 use crate::models::user::find_user_by_username;
 use bcrypt::verify;
+use mongodb::bson::doc;
+use serde::Deserialize;
+use serde_json::{json, Value};
+use chrono::{Duration, Utc, Days};
+
 #[derive(Debug, Deserialize)]
 struct LoginPayload {
     username: String,
@@ -13,9 +19,7 @@ struct LoginPayload {
 }
 
 pub fn routes() -> Router<AppState> {
-    Router::new()
-        .route("/api/auth/signin", post(login))
-
+    Router::new().route("/api/auth/signin", post(login))
 }
 
 async fn login(State(state): State<AppState>, payload: Json<LoginPayload>) -> Result<Json<Value>> {
@@ -26,16 +30,17 @@ async fn login(State(state): State<AppState>, payload: Json<LoginPayload>) -> Re
         Ok(user) => user,
         Err(e) => {
             println!("Error: {:?}", e);
-            return Err(Error::LoginFail)
+            return Err(Error::LoginFail);
         }
     };
     match verify(&payload.password, &user.password) {
         Ok(valid) => {
             if valid {
+                let now = Utc::now().timestamp();
                 let claims = Claims {
                     id: user.id.to_string(), //user.id.to_string(),
                     salt: user.salt.clone(),
-                    exp: 24 * 60 * 60,
+                    exp: add_time(now, 1),
                 };
                 let token = create_token(&claims);
                 println!("User {} successfully logged in", user.username);
@@ -43,13 +48,12 @@ async fn login(State(state): State<AppState>, payload: Json<LoginPayload>) -> Re
                     "id": user.id.to_string(),
                     "username": user.username,
                     "email": user.email,
+                    "roles": process_roles(user.roles),
                     "accessToken": token,
-                    "roles": user.roles.iter().map(|role| role.to_string()).collect::<Vec<String>>(),
                 });
 
                 Ok(Json(doc))
-            }
-            else {
+            } else {
                 println!("Password is invalid");
                 Err(Error::LoginFail)
             }
@@ -58,7 +62,17 @@ async fn login(State(state): State<AppState>, payload: Json<LoginPayload>) -> Re
             println!("Error: {:?}", e);
             Err(Error::LoginFail)
         }
-
     }
+}
 
+fn process_roles(roles: Vec<Role>) -> Vec<String> {
+    roles
+        .iter()
+        .map(|role| format!("ROLE_{}", role.name.to_ascii_uppercase()))
+        .collect::<Vec<String>>()
+}
+
+fn add_time(now: i64, days: i64) -> usize {
+    let sixty_four = now + (days * 86400);
+    sixty_four as usize
 }
