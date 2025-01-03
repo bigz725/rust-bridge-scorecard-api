e665653225c8ff905d78af099e2351042611abc9
Zach Kissane <zkissane@gmail.com>
2025-01-03 10:29:27 -0600

    filtering users works now

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter, Result as FmtResult};
 use sqlx::PgPool;
-use diesel::{prelude::*, r2d2::{ConnectionManager, Pool}};
+use diesel::{associations::HasTable, prelude::*, r2d2::{ConnectionManager, Pool}};
 
 type DieselPool = Pool<ConnectionManager<PgConnection>>;
 
@@ -72,25 +72,36 @@ pub async fn all_users(_db: &PgPool) -> Result<Vec<User>, UserError> {
 #[tracing::instrument(target = "database", skip(diesel_pool))]
 pub async fn find_user(
     diesel_pool: &DieselPool,
-    user_id: Option<&str>,
-    username: Option<&str>,
-    email: Option<&str>,
-    salt: Option<&str>,
+    user_id_param: Option<&str>,
+    username_param: Option<&str>,
+    email_param: Option<&str>,
+    salt_param: Option<&str>,
 ) -> Result<Vec<User>, UserError> {
     use crate::schema::users::dsl::*;
     let mut conn = diesel_pool.clone().get().unwrap();
+    let mut query = users::table().into_boxed();
     
-
+    if let Some(user_id) = user_id_param {
+        query = query.filter(id.eq(Uuid::parse_str(user_id).unwrap()));
+    }
+    if let Some(uname) = username_param {
+        query = query.filter(username.eq(uname));
+    }
+    if let Some(em) = email_param {
+        query = query.filter(email.eq(em));
+    }
+    if let Some(slt) = salt_param {
+        query = query.filter(salt.eq(slt));
+    }
     
-    let results = users
+    query
         .limit(5)
         .select(User::as_select())
         .load::<User>(&mut conn)
         .map_err(|e| {
             tracing::error!("Error: {:?}", e);
             UserError::NoDbConnectionError
-        });
-    results
+        })
 }
 
 pub async fn save_user(_db: &PgPool, _user: &User) -> Result<(), UserError> {
