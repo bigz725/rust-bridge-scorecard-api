27d6e067a4caff2da7e335d8ac16429f8ba67112
Zach Kissane <zkissane@gmail.com>
2025-04-19 11:05:50 -0500

    bump rand crate version, requires code change

--- backend/src/auth/salt.rs
+++ backend/src/auth/salt.rs
@@ -2,7 +2,7 @@ use rand::prelude::*;
 use base64::{engine::general_purpose, Engine as _};
 
 pub async fn salt() -> String {
-    let mut rng = rand::rngs::StdRng::from_entropy();
+    let mut rng = rand::rngs::StdRng::from_os_rng();
     let mut bytes = [0u8; 40]; //TODO: Add the salt length to the config
     rng.fill_bytes(&mut bytes);
     general_purpose::STANDARD.encode(bytes)
